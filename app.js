82e421112be86db29c82d96a5c4e09c90cdfdf62
lucastimho <lucas.t.ho.lh@gmail.com>
2022-06-09 11:09:00 -0500

    adds home starting content

--- app.js
+++ app.js
@@ -16,7 +16,7 @@ app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
 
 app.get("/", function(req, res) {
-  res.render("<h1>Home</h1>");
+  res.render("home", {homeContent: homeStartingContent});
 })
 
 
