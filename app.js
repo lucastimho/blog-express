7d4947c5e4a95082a2fdafb6b2f99bb3e8d462bf
lucastimho <lucas.t.ho.lh@gmail.com>
2022-06-10 12:13:27 -0500

    adds compose page

--- app.js
+++ app.js
@@ -25,7 +25,9 @@ app.get("/about", function (req, res) {
 app.get("/contact", function (req, res) {
   res.render("contact", {contactContent: contactContent})
 })
-
+app.get("/compose", function(req, res) {
+  res.render(__dirname + "/views/compose.ejs");
+})
 
 
 
