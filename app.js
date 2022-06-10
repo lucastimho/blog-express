6939da76636cbc33e978a71602af6aef909880e6
lucastimho <lucas.t.ho.lh@gmail.com>
2022-06-10 13:26:41 -0500

    adds post list

--- app.js
+++ app.js
@@ -17,8 +17,7 @@ app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
 
 app.get("/", function(req, res) {
-  res.render("home", {homeContent: homeStartingContent});
-  console.log(posts)
+  res.render("home", {homeContent: homeStartingContent, postList: posts});
 })
 
 app.get("/about", function (req, res) {
