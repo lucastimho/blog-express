5a2de2cfeb7e34e60a14afaaf67482ea9c7f6621
lucastimho <lucas.t.ho.lh@gmail.com>
2022-06-13 18:03:55 -0500

    adds template

--- app.js
+++ app.js
@@ -3,7 +3,7 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
-const _ = require("lodash");
+const lodash = require("lodash");
 
 const homeStartingContent =
   "Lacus vel facilisis volutpat est velit egestas dui id ornare. Semper auctor neque vitae tempus quam. Sit amet cursus sit amet dictum sit amet justo. Viverra tellus in hac habitasse. Imperdiet proin fermentum leo vel orci porta. Donec ultrices tincidunt arcu non sodales neque sodales ut. Mattis molestie a iaculis at erat pellentesque adipiscing. Magnis dis parturient montes nascetur ridiculus mus mauris vitae ultricies. Adipiscing elit ut aliquam purus sit amet luctus venenatis lectus. Ultrices vitae auctor eu augue ut lectus arcu bibendum at. Odio euismod lacinia at quis risus sed vulputate odio ut. Cursus mattis molestie a iaculis at erat pellentesque adipiscing.";
@@ -27,7 +27,7 @@ app.get("/posts/:title", function (req, res) {
   let requestedTitle = req.params.title;
   let match = 0;
   posts.forEach(function (n) {
-    if (n.title === _.lowerCase(requestedTitle)) {
+    if (n.title === lodash.lowerCase(requestedTitle)) {
       match++;
       console.log(match);
     }
