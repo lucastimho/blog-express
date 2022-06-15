d98cca5aa208fdae48c09773d5507ed304e2c467
lucastimho <lucas.t.ho.lh@gmail.com>
2022-06-14 20:06:54 -0500

    condenses post iteration

--- app.js
+++ app.js
@@ -3,7 +3,7 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
-const lodash = require("lodash");
+const _ = require("lodash");
 
 const homeStartingContent =
   "Lacus vel facilisis volutpat est velit egestas dui id ornare. Semper auctor neque vitae tempus quam. Sit amet cursus sit amet dictum sit amet justo. Viverra tellus in hac habitasse. Imperdiet proin fermentum leo vel orci porta. Donec ultrices tincidunt arcu non sodales neque sodales ut. Mattis molestie a iaculis at erat pellentesque adipiscing. Magnis dis parturient montes nascetur ridiculus mus mauris vitae ultricies. Adipiscing elit ut aliquam purus sit amet luctus venenatis lectus. Ultrices vitae auctor eu augue ut lectus arcu bibendum at. Odio euismod lacinia at quis risus sed vulputate odio ut. Cursus mattis molestie a iaculis at erat pellentesque adipiscing.";
@@ -24,17 +24,18 @@ app.get("/", function (req, res) {
   res.render("home", { homeContent: homeStartingContent, postList: posts });
 });
 app.get("/posts/:title", function (req, res) {
-  let requestedTitle = req.params.title;
-  let match = 0;
-  for (let i = 0; i < posts.length; i++) {
-    if (posts[i].title === lodash.lowerCase(requestedTitle)) {
-      match++;
-      let index = i;
+  const requestedTitle = _.lowerCase(req.params.title);
+
+  posts.forEach(function(post){
+    let storedTitle = _.lowerCase(post.title);
+
+    if (storedTitle === requestedTitle) {
+      res.render("post", {
+        title: post.title,
+        content: post.content
+      });
     }
-  }
-  if (match > 0) {
-    console.log("Match found");
-  }
+  });
 });
 app.get("/about", function (req, res) {
   res.render("about", { aboutContent: aboutContent });
