d88fe0b433057fdd73c957e579184488f5d29c57
lucastimho <lucas.t.ho.lh@gmail.com>
2022-06-23 13:20:25 -0500

    adds in db connection to node.js

--- app.js
+++ app.js
@@ -12,7 +12,6 @@ const aboutContent =
   "Hac habitasse platea dictumst vestibulum rhoncus est pellentesque. Dictumst vestibulum rhoncus est pellentesque elit ullamcorper. Non diam phasellus vestibulum lorem sed. Platea dictumst quisque sagittis purus sit. Egestas sed sed risus pretium quam vulputate dignissim suspendisse. Mauris in aliquam sem fringilla. Semper risus in hendrerit gravida rutrum quisque non tellus orci. Amet massa vitae tortor condimentum lacinia quis vel eros. Enim ut tellus elementum sagittis vitae. Mauris ultrices eros in cursus turpis massa tincidunt dui.";
 const contactContent =
   "Scelerisque eleifend donec pretium vulputate sapien. Rhoncus urna neque viverra justo nec ultrices. Arcu dui vivamus arcu felis bibendum. Consectetur adipiscing elit duis tristique. Risus viverra adipiscing at in tellus integer feugiat. Sapien nec sagittis aliquam malesuada bibendum arcu vitae. Consequat interdum varius sit amet mattis. Iaculis nunc sed augue lacus. Interdum posuere lorem ipsum dolor sit amet consectetur adipiscing elit. Pulvinar elementum integer enim neque. Ultrices gravida dictum fusce ut placerat orci nulla. Mauris in aliquam sem fringilla ut morbi tincidunt. Tortor posuere ac ut consequat semper viverra nam libero.";
-let posts = [];
 
 const app = express();
 
@@ -31,21 +30,25 @@ const postSchema = {
 const Post = mongoose.Model("Post", postSchema);
 
 app.get("/", function (req, res) {
-  res.render("home", { homeContent: homeStartingContent, postList: posts });
-});
-app.get("/posts/:title", function (req, res) {
-  const requestedTitle = _.lowerCase(req.params.title);
-
-  posts.forEach(function(post){
-    let storedTitle = _.lowerCase(post.title);
-
-    if (storedTitle === requestedTitle) {
-      res.render("post", {
-        title: post.title,
-        content: post.content
-      });
+   Post.find({}, function(err, posts) {
+    if (err) {
+      console.log(err);
+    } else {
+      res.render("home", {
+        startingContent: homeStartingContent,
+        posts: posts
+    })
     }
-  });
+   })
+});
+app.get("/posts/:postId", function (req, res) {
+   Item.findOne({_id: req.params.postId}, function(err, post) {
+    if (err) {
+      console.log(err);
+    } else {
+      res.render("post", {postList: post})
+    }
+   })
 });
 app.get("/about", function (req, res) {
   res.render("about", { aboutContent: aboutContent });
